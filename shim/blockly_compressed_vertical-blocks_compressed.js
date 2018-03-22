20a21029bb956dd59fa223b85789b94db9f7502f
action-hong <kkopitehong@gmail.com>
2018-03-22 14:17:41 +0800

    添加blocklys打包配置依赖关系

--- shim/blockly_compressed_vertical-blocks_compressed.js
+++ shim/blockly_compressed_vertical-blocks_compressed.js
@@ -1,1 +1,2 @@
-module.exports = require('imports?Blockly=./shim/blockly_compressed_vertical.Blockly!exports?Blockly!../blocks_compressed');
+// module.exports = require('imports?Blockly=./shim/blockly_compressed_vertical.Blockly!exports?Blockly!../blocks_compressed');
+module.exports = require('imports?Blockly=./shim/blockly_compressed_vertical-lua_compressed!exports?Blockly!../blocks_compressed');
---
+++ shim/blockly_compressed_vertical-javascript_compressed.js
@@ -0,0 +1,2 @@
+// 搞了大半天, 原来是多写了 .js擦  ./shim/blockly_compressed_vertical.Blockly.js
+module.exports = require('imports?Blockly=./shim/blockly_compressed_vertical.Blockly!exports?Blockly!../javascript_compressed');
